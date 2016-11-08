372003f77670d1d6f185a5c7917c542e22f6fb7f
Maxim Kochurov <maxim.v.kochurov@gmail.com>
2016-11-08 13:47:56 +0300

    refactored panel

--- simpanel/panel.py
+++ simpanel/panel.py
@@ -1,18 +1,28 @@
 # coding=utf-8
+from collections import OrderedDict
 import pandas as pd
+import pymc3 as pm
 from simpanel.glm import Glm
 
 
 class SimPanel(object):
-    def __init__(self, X, y, groupid, sim):
+    def __init__(self, name, X, y, sim, idindex=None, idcol=None):
         if (not isinstance(X, pd.DataFrame) or
                 not isinstance(y, pd.Series)):
             raise TypeError('Please provide X as pd.DataFrame and y as pd.Series')
+        if idcol is None or idindex is None:
+            raise ValueError('Please provide idcol or idindex')
+        self.name = name
+        self.groups = OrderedDict()
+        self.advifits = OrderedDict()
         full = pd.concat([X, y], 1)  # type: pd.DataFrame
-        full.set_index(groupid, append=True, inplace=True)
-        self.groups = dict()
-        for label, df in full.groupby(level=groupid):
+        level = idindex
+        if idcol:
+            full.set_index(idcol, append=True, inplace=True)
+            level = idcol
+        for label, df in full.groupby(level=level):
             self.groups[label] = df
+        self.sim = sim
 
     @property
     def data(self):
